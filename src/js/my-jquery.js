9a7ca0326600562ca77b42174b04419720087f20
MizuhoOkimoto <54873998+MizuhoOkimoto@users.noreply.github.com>
2021-09-02 01:32:54 -0400

    changed directory

---
+++ src/js/my-jquery.js
@@ -0,0 +1,87 @@
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides((slideIndex += n));
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides((slideIndex = n));
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("demo");
+//   var captionText = document.getElementById("caption");
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
+//   captionText.innerHTML = dots[slideIndex - 1].p;
+// }
+
+//------ PAGE-TOP BUTTON ------//
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 200) {
+    //上から200pxスクロールしたら
+    $("#page-top").removeClass("RightMove"); //#page-topについているRightMoveというクラス名を除く
+    $("#page-top").addClass("LeftMove"); //#page-topについているLeftMoveというクラス名を付与
+  } else {
+    if ($("#page-top").hasClass("LeftMove")) {
+      //すでに#page-topにLeftMoveというクラス名がついていたら
+      $("#page-top").removeClass("LeftMove"); //LeftMoveというクラス名を除き
+      $("#page-top").addClass("RightMove"); //RightMoveというクラス名を#page-topに付与
+    }
+  }
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on("load", function () {
+  PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// #page-topをクリックした際の設定
+$("#page-top").click(function () {
+  $("body,html").animate(
+    {
+      scrollTop: 0, //ページトップまでスクロール
+    },
+    500
+  ); //ページトップスクロールの速さ。数字が大きいほど遅くなる
+  return false; //リンク自体の無効化
+});
+
+//------ WORKS SECTION ------//
+
+$(".slider").slick({
+  autoplay: false, //自動的に動き出すか。初期値はfalse。
+  infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+  speed: 500, //スライドのスピード。初期値は300。
+  slidesToShow: 3, //スライドを画面に3枚見せる
+  slidesToScroll: 1, //1回のスクロールで1枚の写真を移動して見せる
+  prevArrow: '<div class="slick-prev"></div>', //矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>', //矢印部分NextのHTMLを変更
+  centerMode: true, //要素を中央ぞろえにする
+  variableWidth: true, //幅の違う画像の高さを揃えて表示
+  dots: true, //下部ドットナビゲーションの表示
+});
