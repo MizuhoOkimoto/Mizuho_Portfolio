014dd6a9904267009cf8b02ff2a0d37230a2ee9c
MizuhoOkimoto <54873998+MizuhoOkimoto@users.noreply.github.com>
2021-10-31 00:46:34 -0400

    add resume download function and modified project part

---
+++ src/js/my-jquery.js
@@ -0,0 +1,52 @@
+//------ PAGE-TOP BUTTON ------//
+//the movements when scrolling
+function PageTopAnime() {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 200) {
+    //200px from the top when scrolled
+    $("#page-top").removeClass("RightMove"); //#page-topについているRightMoveというクラス名を除く
+    $("#page-top").addClass("LeftMove"); //#page-topについているLeftMoveというクラス名を付与
+  } else {
+    if ($("#page-top").hasClass("LeftMove")) {
+      //すでに#page-topにLeftMoveというクラス名がついていたら
+      $("#page-top").removeClass("LeftMove"); //LeftMoveというクラス名を除き
+      $("#page-top").addClass("RightMove"); //RightMoveというクラス名を#page-topに付与
+    }
+  }
+}
+
+//when scrolling
+$(window).scroll(function () {
+  PageTopAnime(); /* call the function*/
+});
+
+// how to move when the page is loaded
+$(window).on("load", function () {
+  PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// the settings when #page-top is clicked
+$("#page-top").click(function () {
+  $("body,html").animate(
+    {
+      scrollTop: 0, //scrolling to the top page
+    },
+    500
+  ); //the speed for the page top scroll(bigger number will be slower)
+  return false; //Disable the link itself
+});
+
+//------ WORKS SECTION ------//
+
+$(".slider").slick({
+  autoplay: false, //automatically move?
+  infinite: true, //loop the slide?
+  speed: 500, //the speed for the slide(default is 300)
+  slidesToShow: 3, //how many slide?
+  slidesToScroll: 1, //one img for one scroll
+  prevArrow: '<div class="slick-prev"></div>', //change the html for the Preview
+  nextArrow: '<div class="slick-next"></div>', //change the html for the Next
+  centerMode: true, //center elements
+  variableWidth: true, //Display images with different widths at the same height
+  dots: true, //display dot navigation
+});
